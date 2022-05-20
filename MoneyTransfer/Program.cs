185ec7d6a5c11cd9c75290ebd6a5e3e306874407
Kamil Preweda <preweda.kamil@o2.pl>
2022-05-20 14:41:20 +0200

    Added logic and console UI

--- MoneyTransfer/Program.cs
+++ MoneyTransfer/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using MoneyTransfer;
+
+Guy jacek = new Guy() { Cash = 50, Name = "Jacek" };
+Guy bartek = new Guy() { Cash = 100, Name = "Bartek" };
+
+while (true)
+{
+    jacek.WriteMyInfo();
+    bartek.WriteMyInfo();
+
+    Console.Write("Podaj kwotę: ");
+    string howMuch = Console.ReadLine();
+    if (howMuch == "") return;
+    if (int.TryParse(howMuch, out int amount))
+    {
+        Console.Write("Pieniądze ma przekazać: ");
+        string whichGuy = Console.ReadLine();
+        if (whichGuy == "Jacek")
+        {
+            jacek.GiveCash(amount);
+            bartek.RecieveCash(amount);
+        }
+        else if (whichGuy == "Bartek")
+        {
+            bartek.GiveCash(amount);
+            jacek.RecieveCash(amount);
+        }
+        else
+        {
+            Console.WriteLine("Wpisz 'Jacek', lub 'Bartek'");
+        }
+    
+    }
+    else
+    {
+        Console.WriteLine("Wpisz kwotę (lub pusty wiersz, aby zakończyć).");
+    }
+}
